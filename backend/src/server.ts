fc3b2df92c682f2ea418f06087c6d78dff44384d
shrijithps <p.shrijith@talview.com>
2025-11-16 00:11:48 +0530

    backend_graphql_setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { graphqlHTTP } from "express-graphql";
+import { graphqlHTTP } from 'express-graphql';
 import { schema } from "./graphql/schema.ts";
 import { resolvers } from "./graphql/resolvers.ts";
 
