f2efd9496a9c7549158f51b68e60ec9991af8e2d
shrijithps <p.shrijith@talview.com>
2025-11-16 11:54:49 +0530

    dashboard_details

--- src/components/DashboardView.tsx
+++ src/components/DashboardView.tsx
@@ -1,7 +1,7 @@
 import type {ReactNode} from "react";
+import {Briefcase, FileText, Users} from "lucide-react";
 
 const DashboardView = () => {
-    console.log("data");
 
 interface DashboardCardProps {
      title: string;
@@ -12,51 +12,81 @@ interface DashboardCardProps {
 const DashboardCards = [
      {
         title: "Active Jobs",
-        icon: "",
+        icon: <Briefcase/>,
         count: 0
      },
      {
         title: "Total Applicants",
-        icon: "",
+        icon: <Users/>,
         count: 0
      },
      {
         title: "New Applicants",
-        icon: "",
+        icon: <FileText/>,
         count: 0
-     }
-     ]
+     }]
+
+    const RecentActivity = [
+        {
+            title: "New Applicant",
+            role: "Software Engineer",
+            time: "2 hours ago"
+        },
+        {
+            title: "Job Post Published",
+            role: "Software Engineer",
+            time: "3 hours ago"
+        }
+    ]
 
     const DashboardDetails = ({title, icon, count}: DashboardCardProps) => {
         return (
             <div>
-                <div>
-                    <p>{title}</p>
-                    <p>{icon}</p>
-                    <p>{count}</p>
+                <div className="flex flex-row justify-between items-center">
+                    <div>
+                        <p className="text-gray-500">{title}</p>
+                        <p className="font-bold text-2xl">{count}</p>
+                    </div>
+                    <p className="text-cyan-600 rounded-lg">{icon}</p>
                 </div>
             </div>
         )
     }
 
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Welcome back! Here's an overview of your recruitment activity</p>
+        <div className="p-10">
 
-            <div>
-                {DashboardCards.map((card, index) => (
-                    <DashboardDetails
-                        key={index}
-                        title={card.title}
-                        icon={card.icon}
-                        count={card.count}
-                    />
-                ))}
-            <div>
-                </div>
+            <div className="flex flex-col gap-2">
+                <h1 className="font-bold text-4xl">Dashboard</h1>
+                <p className="text-gray-500 text-center text-xl">Welcome back! Here's an overview of your recruitment activity</p>
             </div>
-        </div>
+
+            <div className="flex flex-row gap-2 p-2">
+                {DashboardCards.map((card, index) => (
+                    <div className="border-gray-400 border-1 rounded-lg w-1/3">
+                        <DashboardDetails
+                            key={index}
+                            title={card.title}
+                            count={card.count}
+                            icon={card.icon}
+                        />
+                    </div>
+                ))}
+            </div>
+
+            {/*Data is hardcoded for now*/}
+            <div>
+                <h1 className="">Recent Activity</h1>
+                {RecentActivity.map((activity, index) => (
+                    <div key={index}>
+                        <p>{activity.title}</p>
+                        <p>{activity.role}</p>
+                        <p>{activity.time}</p>
+                    </div>
+                ))}
+
+            </div>
+       </div>
     );
 };
 
