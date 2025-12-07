a7dc12ab28fed902404bd59e8773a259dcc15f8f
shrijithps <p.shrijith@talview.com>
2025-12-07 10:54:19 +0530

    feat(careerConnect): auth setup

--- src/containers/AuthContainer/auth.tsx
+++
@@ -1,6 +0,0 @@
-import {type ReactNode} from 'react';
-import { AuthProvider } from '../../context/AuthContext';
-
-export const AuthContainer = ({ children }: { children: ReactNode }) => {
-    return <AuthProvider>{children}</AuthProvider>;
-};
