287b152892df3e68dd0132577572c021565994ad
shrijithps <p.shrijith@talview.com>
2025-12-20 11:09:58 +0530

    feat(careerConnect): made the component reusable

--- src/pages/Candidate/JobList.tsx
+++ src/pages/Candidate/JobList.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { Search, MapPin } from "lucide-react";
 import JobCard from "../../components/JobCard";
 import { filteredJobs } from "../../utils/filterJobs";
 import type {Job} from "../../data/jobs";
+import {SearchBar} from "../../components/SearchBar";
 
 const JobsPage = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -34,34 +34,12 @@ const JobsPage = () => {
         </h1>
 
         {/* Search Bar */}
-        <div className="bg-white rounded-lg shadow-lg p-6 mb-8">
-          <div className="grid md:grid-cols-3 gap-4">
-            <div className="flex items-center gap-3 p-3 border border-gray-300 rounded-lg">
-              <Search className="h-5 w-5 text-gray-400" />
-              <input
-                type="text"
-                placeholder="Job title, keywords..."
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                className="flex-1 outline-none text-sm"
-              />
-            </div>
-            <div className="flex items-center gap-3 p-3 border border-gray-300 rounded-lg">
-              <MapPin className="h-5 w-5 text-gray-400" />
-              <input
-                type="text"
-                placeholder="City or remote"
-                value={locationQuery}
-                onChange={(e) => setLocationQuery(e.target.value)}
-                className="flex-1 outline-none text-sm"
-              />
-            </div>
-            <button className="bg-cyan-600 text-white rounded-lg px-6 py-3 font-medium hover:bg-cyan-700 transition-colors flex items-center justify-center gap-2">
-              <Search className="h-5 w-5" />
-              Search Jobs
-            </button>
-          </div>
-        </div>
+        <SearchBar
+            searchQuery={searchQuery}
+            setSearchQuery={setSearchQuery}
+            locationQuery={locationQuery}
+            setLocationQuery={setLocationQuery}
+        />
 
         <div className="flex gap-8">
           {/* Filters Sidebar */}
@@ -117,8 +95,8 @@ const JobsPage = () => {
               Showing {filteredJobs.length} jobs
             </p>
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-              {filteredJobs({searchQuery, locationQuery, selectedJobType, selectedLocation}).map((job:Job) => (
-                <JobCard job={job} />
+              {filteredJobs({searchQuery, locationQuery, selectedJobType, selectedLocation}).map((job:Job, index:number) => (
+                <JobCard key={index} job={job} />
               ))}
             </div>
           </div>
